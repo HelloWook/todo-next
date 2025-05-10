819f5f6d031bf844c7427c82c8e62a37c5729bb4
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-05-11 02:16:01 +0900

    design : input 스타일 수정

--- src/app/common/components/Input/Input.tsx
+++ src/app/common/components/Input/Input.tsx
@@ -1,14 +1,21 @@
+import clsx from 'clsx';
 import React, { InputHTMLAttributes } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     className?: string;
 }
 
-const Input = ({ className }: InputProps) => {
+const Input = ({ className, ...rest }: InputProps) => {
     return (
         <>
             <div className="absolute  w-[1000px] h-[56px] px-6 py-[19px]  bg-slate-900 text-slate-900 rounded-3xl border-2 border-slate-900 translate-1" />
-            <input className="relative w-[1000px] h-[56px] px-6 py-[19px] bg-slate-100 rounded-3xl border-2 border-slate-900 text-slate-500 focus:outline-0"></input>
+            <input
+                className={clsx(
+                    'relative w-[1000px] h-[56px] px-6 py-[19px] bg-slate-100 rounded-3xl border-2 border-slate-900 text-slate-500 focus:outline-0',
+                    className
+                )}
+                {...rest}
+            ></input>
         </>
     );
 };
