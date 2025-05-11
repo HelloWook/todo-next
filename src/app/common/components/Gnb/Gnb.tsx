f2b12977841e43d0018492769352c19b80186d97
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-05-12 01:51:10 +0900

    feat  : gnb 홈으로가는 라우팅 기능 개발

--- src/app/common/components/Gnb/Gnb.tsx
+++ src/app/common/components/Gnb/Gnb.tsx
@@ -1,15 +1,27 @@
+'use client';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
 import React from 'react';
 
 const Gnb = () => {
+    const router = useRouter();
+
+    const logoClick = () => {
+        router.push('/');
+    };
+
     return (
-        <header className="w-full h-[60px] flex items-center border-b-1 border-b-slate-200 bg-white  mb-[22px]">
+        <header className="w-full h-[60px] flex items-center border-b border-b-slate-200 bg-white ">
             <div className="max-w-[1200px] m-auto w-full">
                 <Image
                     src={'/logo/big-logo.svg'}
                     alt="로고"
                     width={151}
                     height={54}
+                    onClick={logoClick}
+                    className="cursor-pointer"
+                    unoptimized
                 />
             </div>
         </header>
